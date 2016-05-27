2cb5ff7ca2bc82bee0c019885ba912d7eeed7306
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-05-27 23:01:42 +0100

    Add other features to app

--- app.py
+++ app.py
@@ -7,50 +7,76 @@ import db
 ############################################################################
 # Simple pages
 
-@route('/')
+@route("/")
 def home():
     return template("home")
 
-@route('/about')
+@route("/about")
 def about():
     return template("about")
 
 ############################################################################
 # Dynamic pages
 
-@route('/write')
+@route("/write", method="GET")
 def message_create():
-    if request.method == "POST":
-        msg = {
-        }
+    msg = {
+        "title": "",
+        "text": "",
+        "author": "",
+    }
+    return template("message_edit", msg=msg, errors=[])
+
+@route("/write", method="POST")
+def do_message_create():
+    msg = {
+        "title": request.forms.get("title"),
+        "text": request.forms.get("text"),
+        "author": request.forms.get("author"),
+        "private": request.forms.get("private") == "checked",
+        "time": db.timestamp(),
+    }
+    errors = []
+
+    if not msg["title"] and not msg["text"]:
+        errors.append("Please supply a title or message")
+
+    if not errors:
+        msg = db.collection_insert("msg", msg)
+        return template("message_success", msg=msg)
     else:
-        msg = {
-            "title": "",
-            "text": "",
-            "author": "",
-        }
+        return template("message_edit", msg=msg, errors=errors)
 
-    return template("message_edit", message=msg)
-
-@route('/messages')
+@route("/messages")
 def message_list():
-    msgs = db.get_collection("msg")
-    return template("message_list", messages=msgs)
+    msgs = db.get_collection("msg").values()
+    return template("message_list", msgs=msgs)
 
-@route('/messages/<id>')
+@route("/messages/<id>")
 def message_detail(id=None):
     msgs = db.get_collection("msg")
     msg = msgs.get(id)
     if msg:
-        return template("message_detail", message=msg)
+        return template("message_detail", msg=msg)
     else:
         abort(404, "Message not found")
 
-@route('/static/<path:path>')
+@route("/stats")
+def stats():
+    msgs = db.get_collection("msg")
+    stats = {
+        "count": len(msgs),
+        "num_authors": len(set(msg.get("author") for msg in msgs.values())),
+        "longest": max(len(msg.get("text", "")) for msg in msgs.values()),
+    }
+    return template("stats", **stats)
+
+
+@route("/static/<path:path>")
 def callback(path):
     return static_file(path, root=os.path.join(os.path.dirname(__file__), "static"))
 
 application = default_app()
 
 if __name__ == "__main__":
-    run(host='localhost', port=8080, debug=True)
+    run(host="localhost", port=8080, debug=True)
--- db.py
+++ db.py
@@ -10,7 +10,55 @@ import pickle
 import fcntl
 import uuid
 import re
+from collections import OrderedDict
+import datetime
 from bottle import app
+
+storage = {}
+
+def load_file(name):
+    try:
+        with open(db_path(name), "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        pass
+    except OSError as e:
+        print("Error opening database %s: %s" % (name, e))
+    return None
+
+def save_file(name, data):
+    try:
+        os.makedirs(db_dir, exist_ok=True)
+        with open(db_path(name), "wb") as f:
+            pickle.dump(data, f)
+    except OSError as e:
+        print("Error saving database %s: %s" % (name, e))
+
+db_dir = os.path.join(os.path.dirname(__file__), "_db")
+
+def db_path(name):
+    return os.path.join(db_dir, name)
+
+def get_collection(name):
+    global storage
+    if name not in storage:
+        storage[name] = load_file(name) or OrderedDict()
+    return storage[name]
+
+def put_collection(name, data):
+    global storage
+    storage[name] = data
+    save_file(name, data)
+
+def collection_insert(name, item):
+    col = get_collection(name)
+    item["id"] = make_uuid()
+    col[item["id"]] = item
+    put_collection(name, col)
+    return item
+
+def timestamp():
+    return datetime.datetime.now().isoformat()
 
 def lock_file(fd):
     fcntl.lockf(fd, fcntl.LOCK_EX)
@@ -20,7 +68,6 @@ def unlock_file(fd):
 
 def make_uuid():
     return str(uuid.uuid4())
-
 
 def uuid_filter(config):
     "Matches hexadecimal formatted 16 byte UUID"

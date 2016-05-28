cb90b35cf4a6fa1e25a1e5b7090ed9256b2fdc09
Rodney Lorrimar <dev@rodney.id.au>
2016-05-28 12:38:36 +0100

    Remove unused code from db.py

--- db.py
+++ db.py
@@ -1,18 +1,20 @@
 """
-This is a really simple data persistence module.
+This is a highly unsophisticated data persistence module.
 
 It works by unpickling and pickling normal Python data structures.
+
+For large amounts of data or multi-threaded servers, you will need to
+use something better, like sqlite.
 """
 
+import datetime
 import os
 import os.path
 import pickle
-import fcntl
-import uuid
 import re
-from collections import OrderedDict
-import datetime
+import uuid
 from bottle import app
+from collections import OrderedDict
 
 storage = {}
 
@@ -60,25 +62,5 @@ def collection_insert(name, item):
 def timestamp():
     return datetime.datetime.now().isoformat()
 
-def lock_file(fd):
-    fcntl.lockf(fd, fcntl.LOCK_EX)
-
-def unlock_file(fd):
-    fcntl.lockf(fd, fcntl.LOCK_UN)
-
 def make_uuid():
     return str(uuid.uuid4())
-
-def uuid_filter(config):
-    "Matches hexadecimal formatted 16 byte UUID"
-    regexp = r"[0-9a-f]{8}(-[0-9a-f]{4}){4}[0-9a-f]{8}"
-
-    def to_python(match):
-        return match.group(0)
-
-    def to_url(uuid):
-        return str(uuid)
-
-    return regexp, to_python, to_url
-
-# app.router.add_filter('uuid', db.uuid_filter)
